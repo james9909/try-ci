bbeb20fca4246f373ef13b0c96dece574dc79372
Anya Keller <anyakeller@betelgeuse.local>
2017-02-22 14:36:55 -0500

    fixed function call in test_add

--- tests/test_add.py
+++ tests/test_add.py
@@ -3,5 +3,5 @@ import json
 class TestAdd():
 
     def test_add(self, client):
-        response = client.get("/fib", data={"a": 1,"b": 2})
+        response = client.get("/add", data={"a": 1,"b": 2})
         assert json.loads(response.data)["result"] == 3
