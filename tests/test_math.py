de0dadfb6269f64dca3b054308657887281ab2c1
Anya Keller <anyakeller@betelgeuse.local>
2017-03-08 13:39:47 -0500

    added fib route

--- tests/test_math.py
+++ tests/test_math.py
@@ -42,3 +42,41 @@ class TestMath():
             }
             response = client.get("/add", data=data)
             assert json.loads(response.data)["result"] == test["expected"]
+ 
+    def test_fib(self, client):
+        tests = [
+            {
+                "n": 0,
+                "expected": 0
+            },
+            {
+                "n": 1,
+                "expected": 1
+            },
+            {
+                "n": 2,
+                "expected": 1
+            },
+            {
+                "n": 3,
+                "expected": 2
+            },
+            {
+                "n": 10,
+                "expected": 55
+            },
+            {
+                "n": 20,
+                "expected": 6765
+            },
+            {
+                "n": 50,
+                "expected": 12586269025
+            }
+        ]
+        for test in tests:
+            data = {
+                "n": test["n"]
+            }
+            response = client.get("/fib", data=data)
+            assert json.loads(response.data)["result"] == test["expected"]
